b03bb6a25f5fed8fe30f0448e35568f9bd72aed3
Чёрный Ярослав Александрович <217049@edu.fa.ru>
2023-10-31 16:24:54 +0300

    Автодополнение

---
+++ Автодполнение/Автодополнение.js
@@ -0,0 +1,42 @@
+let arr  = [ 'Bulgaria', "Croatia", "Czech Republic", "Denmark", "Estonia",];
+const input = document.getElementById("elem");
+const ul = document.querySelector("#list");
+
+input.addEventListener("input", () => {
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     if(arr[i].toLowerCase().includes(input.value.toLowerCase())) {
+    //         const li = document.createElement("li");
+    //         li.textContent = arr[i];
+
+    //         li.addEventListener("click", () => {
+    //             input.value = li.textContent;
+    //         });
+    //         ul.appendChild(li);
+    //     } else {
+    //         ul.innerHTML = "";
+    //     }
+        
+    // }
+    // if(input.value === "") {
+    //     ul.innerHTML = "";
+    // }
+    ul.innerHTML = "";
+
+    const newArr = arr.filter((item) => {
+        if(item.startsWith(input.value)) {
+            return true;
+        }
+    });
+
+    newArr.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item;
+
+        li.addEventListener("click", () => {
+            input.value = item;
+        });
+
+        ul.appendChild(li);
+    });
+});
